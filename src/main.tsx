e1fe5955b5a8132fe2179dff3462f6a4bfb010cd
mpbusinesscash <mpbusinesscash@gmail.com>
2024-07-19 19:13:01 +0000

    Removed unused imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
